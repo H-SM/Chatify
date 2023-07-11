ce6c3fbf07f4016577fd59c55a1a5f1fb2386b95
Harman Singh <98532264+H-SM@users.noreply.github.com>
2023-07-11 12:14:30 +0530

    completed the UI for the Authentication Page

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,6 +2,8 @@
 
 import Button from "@/app/components/Button";
 import  Input  from "@/app/components/inputs/input";
+import AuthSocialButton from './AuthSocialButton';
+import { BsGithub , BsGoogle } from 'react-icons/bs';
 import { useCallback, useState } from "react";
 import { FieldValues , useForm , SubmitHandler} from "react-hook-form";
 type Varient = 'LOGIN' | 'REGISTER';
@@ -68,6 +70,7 @@ const AuthForm = () =>{
                         label="Name" 
                         register={register}
                         errors={errors}
+                        disabled={isLoading}
                     />
                     )}
                     <Input 
@@ -76,6 +79,7 @@ const AuthForm = () =>{
                             type="email"
                             register={register}
                             errors={errors}
+                            disabled={isLoading}
                     />
                     <Input 
                             id="password" 
@@ -83,6 +87,7 @@ const AuthForm = () =>{
                             type="password"
                             register={register}
                             errors={errors}
+                            disabled={isLoading}
                     />
                     <div>
                         <Button
@@ -104,6 +109,27 @@ const AuthForm = () =>{
                                 Or continue with
                             </span>
                         </div>
+                    </div>
+                    
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialButton 
+                        icon={BsGithub}
+                        onClick={() => socialAction('github')}
+                        />
+                        
+                        <AuthSocialButton 
+                        icon={BsGoogle}
+                        onClick={() => socialAction('google')}
+                        />
+                    </div>
+                </div>
+
+                <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+                    <div>
+                        {variant === 'LOGIN' ? "New to Messenger?" : "Already have an account?"}
+                    </div>
+                    <div onClick={toggleVarient} className="underline cursor-pointer">
+                        {variant === 'LOGIN' ? "Create an account" : "Log-in"}
                     </div>
                 </div>
             </div>
