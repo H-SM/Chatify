25f3a128179f50be3c08e6d96bbf1dd36aa0f5a0
Harman Singh <98532264+H-SM@users.noreply.github.com>
2023-07-17 16:57:35 +0530

    added avatar to the sidebar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -3,11 +3,19 @@
 import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
 import DesktopItem from "./DesktopItem";
+import { User } from "@prisma/client";
+import Avatar from "../Avatar";
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+    currentUser: User,
+}
+const DesktopSidebar : React.FC<DesktopSidebarProps>= ({
+    currentUser
+}) => {
     const routes = useRoutes();
     const [isOpen , setIsOpen] = useState(false);
 
+    console.log({currentUser});
     return ( 
         <div className="
             hidden
@@ -42,6 +50,24 @@ const DesktopSidebar = () => {
                         />
                     ))}
                 </ul>
+             </nav>
+             <nav className="
+                mt-4
+                flex
+                flex-col
+                justify-between
+                items-center
+             ">
+                <div
+                    onClick={() => setIsOpen(true)}
+                    className="
+                        cursor-pointer
+                        hover:opacity-75
+                        transition
+                    "
+                >
+                    <Avatar user={currentUser}/>
+                </div>
              </nav>
         </div>
     )
--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,12 +1,16 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
 import DesktopSidebar from "@/app/components/sidebar/DesktopSidebar";
 import MobileFooter from "@/app/components/sidebar/MobileFooter";
 
 async function Sidebar({ children}: {
     children : React.ReactNode
 }) {
+    const currentUser = await getCurrentUser();
+
     return ( 
         <div className="h-full">
-            <DesktopSidebar/>
+            {/* npm install next-superjson-plugin */}
+            <DesktopSidebar currentUser={currentUser!}/>
             <MobileFooter/>
             <main className="lg:pl-20 h-full">
                 {children}
